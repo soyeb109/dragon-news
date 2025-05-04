38cd1d1114d93c084ad1df35a1e5077a6b17b57c
Md. Soyeb Islam <soyeb10191@gmail.com>
2025-05-04 23:14:38 +0600

    load news data & filter news data based on category

--- src/pages/CategoryNews.jsx
+++ src/pages/CategoryNews.jsx
@@ -1,9 +1,27 @@
-import React from "react";
-import { useParams } from "react-router";
+import React, { useEffect, useState } from "react";
+import { useLoaderData, useParams } from "react-router";
 
 const CategoryNews = () => {
   const { id } = useParams();
-  return <div>CategoryNews - {id}</div>;
+  const data = useLoaderData();
+  const [categoryNews, setCategoryNews] = useState([]);
+
+  useEffect(() => {
+    if (id == 0) {
+      setCategoryNews(data);
+      return;
+    } else if (id == "1") {
+      const filteredNews = data.filter(
+        (news) => news.others.is_today_pick == true
+      );
+      setCategoryNews(filteredNews);
+    } else {
+      const filteredNews = data.filter((news) => news.category_id == id);
+      setCategoryNews(filteredNews);
+    }
+  }, [id, data]);
+
+  return <div>Total {categoryNews.length}</div>;
 };
 
 export default CategoryNews;
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -14,6 +14,7 @@ const router = createBrowserRouter([
       },
       {
         path: "/category/:id",
+        loader: () => fetch("/news.json"),
         Component: CategoryNews,
       },
     ],
