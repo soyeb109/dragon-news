5793f87217a452722f7cbca3c23ec9e3f97e7326
Md. Soyeb Islam <soyeb10191@gmail.com>
2025-05-05 00:45:43 +0600

    add newscard

--- src/pages/CategoryNews.jsx
+++ src/pages/CategoryNews.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useLoaderData, useParams } from "react-router";
+import NewsCard from "../components/NewsCard";
 
 const CategoryNews = () => {
   const { id } = useParams();
@@ -21,7 +22,19 @@ const CategoryNews = () => {
     }
   }, [id, data]);
 
-  return <div>Total {categoryNews.length}</div>;
+  return (
+    <div>
+      <h2 className="font-bold mb-5">
+        Total <span className="text-secondary">{categoryNews.length} </span>News
+        Found
+      </h2>
+      <div className="grid grid-cols-1 gap-5">
+        {categoryNews.map((news) => (
+          <NewsCard key={news.id} news={news} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default CategoryNews;
