b3bef465e026a00f3463f91f4946e52125a272c3
Md. Soyeb Islam <soyeb10191@gmail.com>
2025-05-04 19:28:51 +0600

    implement marquee header

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Outlet } from "react-router";
 import Header from "../components/Header";
+import LatestNews from "../components/LatestNews";
 
 const HomeLayout = () => {
   return (
     <div>
       <header>
         <Header />
+        <section className="w-11/12 mx-auto my-3">
+          <LatestNews />
+        </section>
       </header>
       <main>
         <section className="left_nav"></section>
