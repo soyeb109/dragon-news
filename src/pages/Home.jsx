022d1c0adf889928adf721ffdc7fae48645c5dec
Md. Soyeb Islam <soyeb10191@gmail.com>
2025-05-05 09:13:46 +0600

    sticky scroll behavior

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Navigate } from "react-router";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Navigate to="/category/0" />
+    </div>
+  );
 };
 
 export default Home;
